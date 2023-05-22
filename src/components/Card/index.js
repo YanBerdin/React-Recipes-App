ced769a62ac39078ba57033c0464946faacca0c0
YanBerdin <yandevformation@gmail.com>
2023-05-22 19:24:05 +0200

    maintenant que le state est visible remplacer propriété level qui n'existe pas

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -2,17 +2,20 @@ import PropTypes from 'prop-types';
 
 import './style.scss';
 
+
+// Maintenant que le state est visible dans Redux Dev Tools
+// on peut voir que la propriété level n'existe pas mais difficulty oui
 const Card = ({
   thumbnail,
   title,
-  level,
+  difficulty,
   slug,
 }) => (
   <article className="card">
     <img className="card-img" src={thumbnail} alt={title} />
     <div className="card-content">
       <h2 className="card-title">{title}</h2>
-      <p className="card-desc">Difficulté : {level}</p>
+      <p className="card-desc">Difficulté : {difficulty}</p>
       <a href={`/recipe/${slug}`} className="card-link">Voir la recette</a>
     </div>
   </article>
@@ -21,7 +24,7 @@ const Card = ({
 Card.propTypes = {
   thumbnail: PropTypes.string.isRequired,
   title: PropTypes.string.isRequired,
-  level: PropTypes.string.isRequired,
+  difficulty: PropTypes.string.isRequired,
   slug: PropTypes.string.isRequired,
 };
 
