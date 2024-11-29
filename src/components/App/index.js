ae58fa4fa942807bcdc2ffa33b71af9d800b158e
YanBerdin <yandevformation@gmail.com>
2024-11-29 17:46:03 +0100

    add cleanup with abortController + try catch to fetchRecipes

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,6 +1,8 @@
 import PropTypes from "prop-types";
 import { Routes, Route } from "react-router-dom";
 import { useEffect } from "react";
+import axios from "axios";
+import { useDispatch, useSelector } from "react-redux";
 
 import Menu from "src/components/Menu";
 import Home from "src/components/Home";
@@ -9,19 +11,42 @@ import Error from "src/components/Error";
 
 import "./style.scss";
 
-import axios from "axios";
-import { useDispatch, useSelector } from "react-redux";
 import { setRecipes } from "../../actions/recipes";
+
 import Loading from "./Loading";
 
 function App(props) {
   const dispatch = useDispatch();
   const recipes = useSelector((state) => state.recipes.list);
 
+/*
   useEffect(() => {
     axios.get("http://localhost:3001/api/recipes").then((response) => {
       dispatch(setRecipes(response.data)); // dispatch de l’intention d’action
     });
+  }, []);
+*/
+
+  // https://developer.mozilla.org/en-US/docs/Web/API/AbortController
+  useEffect(() => {
+    console.log("Effet exécuté"); //TODO: à supprimer
+    const abortController = new AbortController();
+    const fetchRecipes = async () => {
+      try {
+        const response = await axios.get("http://localhost:3001/api/recipes", {
+          signal: abortController.signal, // Passer le signal d'abandon à la requête
+        });
+        dispatch(setRecipes(response.data)); // dispatch de l’intention d’action
+      } catch (error) {
+        console.error("Erreur de récupération des recettes", error);
+      }
+    };
+    fetchRecipes();
+    // Cleanup
+    return () => {
+      abortController.abort();
+      console.log("Cleanup exécutée"); //TODO: à supprimer
+    };
   }, []);
 
   if (recipes.length === 0) {
--- src/reducers/user.js
+++ src/reducers/user.js
@@ -2,14 +2,10 @@ import { CHANGE_LOGIN_FIELD, SAVE_LOGIN_SUCCESSFUL } from "../actions/user";
 
 export const initialState = {
   logged: false,
-  // contenu du champ email du formulaire de login
   email: "",
-  // contenu du champ mot de passe du formulaire de login
   password: "",
-  // pseudo de l'utilisateur (une fois connecté)
   nickname: "",
-  // token JWT de l'utilisateur (une fois connecté)
-  token: null,
+  token: null, // JWT
 };
 
 const reducer = (state = initialState, action = {}) => {
@@ -41,10 +37,8 @@ const reducer = (state = initialState, action = {}) => {
         logged: true,
         nickname: action.payload.nickname,
         token: action.payload.token,
-
-        //! sécurité : supprimer les identifiants du state
         email: "",
-        password: "",
+        password: "", //! sécurité : supprimer les identifiants du state
       };
 
     default:
